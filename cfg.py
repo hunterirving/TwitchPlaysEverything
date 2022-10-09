cc9918e2bb05b4f5dcfed835a544917c2a1bef8b
Hunter Irving <8bitvelociraptor@gmail.com>
2022-10-09 18:36:13 -0400

    Create cfg.py

---
+++ cfg.py
@@ -0,0 +1,12 @@
+#A twitch.tv username
+NICK = "usernamehere"
+
+#The name of the twitch.tv channel you want to monitor, preceded by a "#"
+CHAN = "#usernamehere"
+
+#Your unique oauth token, obtained from www.twitchapps.com/tmi
+PASS = "oauthtokenhere"
+
+#These values do not need to be modifed
+HOST = "irc.twitch.tv"
+PORT = 6667
